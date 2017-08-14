a632ed9c5ec49ee9332a301c8f2347693085b8fc
Denis <tendomail@mail.ru>
2017-08-14 21:09:30 +0300

    added loading screen

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { array } from 'prop-types';
 
 import Card from './Card';
+import Loading from './Loading';
 
 Grid.propTypes = {
   cards: array.isRequired
 }
 
 function Grid ({cards}) {
+  if (!cards.length) {
+    return <Loading />
+  }
   return (
     <section className="grid">
       {cards.map((card) =>
