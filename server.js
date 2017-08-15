b0783aea0665535bc8011344eafca6d4d55d0539
Denis <tendomail@mail.ru>
2017-08-15 16:38:19 +0300

    added imports for production

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var cards = require('./service/cards');
 var io = require('socket.io')(http);
 var cors = require('cors');
+var path = require('path');
 
 var startActivity = require('./service/fakeActivity');
 var { getCardsDelay } = require('./service/delay');
 
 app.use(cors());
+//app.use(express.static(path.join(__dirname, 'build')));
 
 // Change public to build in production. Cors importing only needed in development.
 app.get('/', function(req, res) {
-  res.sendFile(path.join( __dirname, '../client/public/index.html'));
+  res.sendFile(path.join( __dirname, './client/public/index.html'));
 });
 
 app.get('/cards', function(req, res){
